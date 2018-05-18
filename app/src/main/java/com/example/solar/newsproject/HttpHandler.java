967b0679f33835f9bfb7963e552deb0938e3e471
Carlos Cano <cdcanos@hotmail.com>
2018-05-18 16:15:55 -0500

    last version

--- app/src/main/java/com/example/solar/newsproject/HttpHandler.java
+++ app/src/main/java/com/example/solar/newsproject/HttpHandler.java
@@ -10,32 +10,42 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
-public class HttpHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-    private static final String TAG = HttpHandler.class.getSimpleName();
+public class HttpHandler {
 
     public HttpHandler() {
     }
 
-    public String makeServiceCall(String reqUrl) {
-        String response = null;
+    public String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
         try {
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            // read the response
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            response = convertStreamToString(in);
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "MalformedURLException: " + e.getMessage());
-        } catch (ProtocolException e) {
-            Log.e(TAG, "ProtocolException: " + e.getMessage());
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();
+            inputStream = urlConnection.getInputStream();
+            jsonResponse = convertStreamToString(inputStream);
+
         } catch (IOException e) {
-            Log.e(TAG, "IOException: " + e.getMessage());
-        } catch (Exception e) {
-            Log.e(TAG, "Exception: " + e.getMessage());
+            //handle the exception
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            return jsonResponse;
         }
-        return response;
     }
 
     private String convertStreamToString(InputStream is) {
--- app/src/main/java/com/example/solar/newsproject/MainActivity.java
+++ app/src/main/java/com/example/solar/newsproject/MainActivity.java
@@ -1,54 +1,90 @@
 package com.example.solar.newsproject;
 
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import android.widget.ListAdapter;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements  AdapterView.OnItemClickListener{
+    private ArrayList<NewData> list1;
+    private ListView lvNews;
+    private NewsAdapter adapter;
+    private TextView tvtitle, tvcontent, tvlink;
     private String TAG = MainActivity.class.getSimpleName();
-    private ListView lv;
-
-    ArrayList<NewData> newsList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        newsList = new ArrayList<>();
-        lv = (ListView) findViewById(R.id.lv01);
 
-        new GetContacts().execute();
+        //ListView Code
+        list1 = new ArrayList<NewData>();
+        new GetNews().execute();
+
+        //fillSongsList();
+        tvtitle = findViewById(R.id.title);
+        tvcontent = findViewById(R.id.textContent);
+        tvlink = findViewById(R.id.link);
+        adapter = new NewsAdapter(this, list1);
+        lvNews = findViewById(R.id.lv01);
+        lvNews.setAdapter(adapter);
+        lvNews.setOnItemClickListener(this);
+
+
+
     }
 
-    private class GetContacts extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Toast.makeText(MainActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+    private void fillSongsList(){
 
-        }
+        list1.add(new NewData("Aire","Golden Ganga","Exitos"));
+        list1.add(new NewData("Animal","Ainda Duo","Vendaval"));
+        list1.add(new NewData("Tormenta","Alex Andwanter","Rebeldes"));
+        list1.add(new NewData("Cosas infinitas","Bengala","Bajo la sal"));
+        list1.add(new NewData("Viva la vida","Coldplay","Viva la vida"));
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        tvtitle.setText(list1.get(i).getTitle());
+        tvcontent.setText(list1.get(i).getContent());
+        tvlink.setText(list1.get(i).getLink());
+    }
+
+    private class GetNews extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... arg0) {
             HttpHandler sh = new HttpHandler();
             // Making a request to url and getting response
-            String url = "http://api.androidhive.info/contacts/";
-            String jsonStr = sh.makeServiceCall(url);
+            String url = "https://content.guardianapis.com/search?api-key=ebecd602-f289-44fb-9f50-ff28ffab0a44";
+            String jsonStr = "";
+
+            try {
+                jsonStr = sh.makeHttpRequest(createUrl(url));
+            } catch (IOException e) {
+                return null;
+            }
 
             Log.e(TAG, "Response from url: " + jsonStr);
             if (jsonStr != null) {
@@ -56,20 +92,21 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject jsonObj = new JSONObject(jsonStr);
 
                     // Getting JSON Array node
-                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+                    JSONObject response = jsonObj.getJSONObject("response");
+                    JSONArray results = response.getJSONArray("results");
 
-                    // looping through All Contacts
-                    for (int i = 0; i < contacts.length(); i++) {
-                        JSONObject c = contacts.getJSONObject(i);
-                        String id = c.getString("id");
-                        String name = c.getString("name");
-                        String email = c.getString("email");
+                    // looping through All results
+                    for (int i = 0; i < results.length(); i++) {
+                        JSONObject r = results.getJSONObject(i);
+                        String id = r.getString("id");
+                        String name = r.getString("webTitle");
+                        String email = r.getString("webUrl");
 
                         // tmp hash map for single contact
-                        NewData nueva = new NewData(id,name,email);
+                        NewData nueva = new NewData(id, name, email);
 
                         // adding contact to contact list
-                        newsList.add(nueva);
+                        list1.add(nueva);
                     }
                 } catch (final JSONException e) {
                     Log.e(TAG, "Json parsing error: " + e.getMessage());
@@ -99,18 +136,16 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
 
-
-        private URL createUrl(String stringUrl) {
-            URL url = null;
-            try {
-                url = new URL(stringUrl);
-            } catch (MalformedURLException exception) {
-                return null;
-            }
-            return url;
-        }
-
-
-
     }
+
+    private URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException exception) {
+            return null;
+        }
+        return url;
+    }
+
 }
